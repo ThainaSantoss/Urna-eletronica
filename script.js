2895a349b864ace1d395f5245029477133d52048
ThainaSantoss <thainados_santos@hotmail.com>
2024-09-23 20:54:32 -0300

    javascript changes

--- script.js
+++ script.js
@@ -7,15 +7,16 @@ let numeros = document.querySelector(".d-1-3");
 
 let etapaAtual = 0;
 let numero = "";
-let votoBranco = false; 
+let votoBranco = false;
 let votos = [];
+let chegouAoFinal = false;
 
 function comecarEtapa() {
   let etapa = etapas[etapaAtual];
 
   let numeroHtml = "";
-  numero = '';
- votoBranco = false;
+  numero = "";
+  votoBranco = false;
 
   for (let i = 0; i < etapa.numeros; i++) {
     if (i === 0) {
@@ -31,7 +32,6 @@ function comecarEtapa() {
   aviso.style.display = "none";
   lateral.innerHTML = "";
   numeros.innerHTML = numeroHtml;
-
 }
 
 function atualizaInterface() {
@@ -51,18 +51,16 @@ function atualizaInterface() {
     aviso.style.display = "block";
     descricao.innerHTML = `Nome: ${candidato.nome}<br/>Partido: ${candidato.partido}`;
 
-
     let fotosHtml = "";
     for (let i in candidato.fotos) {
-        if(candidato.fotos[i].small) {
-      fotosHtml += `<div class="d-1-image small"> <img src="images/${candidato.fotos[i].url}" alt=""/>${candidato.fotos[i].legenda}</div>`;
-        } else {
-      fotosHtml += `<div class="d-1-image"> <img src="images/${candidato.fotos[i].url}" alt=""/>${candidato.fotos[i].legenda}</div>`;
-        }
+      if (candidato.fotos[i].small) {
+        fotosHtml += `<div class="d-1-image small"> <img src="images/${candidato.fotos[i].url}" alt=""/>${candidato.fotos[i].legenda}</div>`;
+      } else {
+        fotosHtml += `<div class="d-1-image"> <img src="images/${candidato.fotos[i].url}" alt=""/>${candidato.fotos[i].legenda}</div>`;
+      }
     }
 
     lateral.innerHTML = fotosHtml;
-
   } else {
     seuVotoPara.style.display = "block";
     aviso.style.display = "block";
@@ -71,7 +69,6 @@ function atualizaInterface() {
 }
 
 function clicou(n) {
-
   let elNumero = document.querySelector(".numero.pisca");
   if (elNumero !== null) {
     elNumero.innerHTML = n;
@@ -88,49 +85,63 @@ function clicou(n) {
 }
 
 function branco() {
-    numero = '';
-    votoBranco = true;
-    seuVotoPara.style.display = "block";
-    aviso.style.display = "block";
-    numeros.innerHTML = '';
-    descricao.innerHTML = '<div class="aviso--grande pisca">VOTO EM BRANCO</div>';
-    lateral.innerHTML = '';
+  numero = "";
+  votoBranco = true;
+  seuVotoPara.style.display = "block";
+  aviso.style.display = "block";
+  numeros.innerHTML = "";
+  descricao.innerHTML = '<div class="aviso--grande pisca">VOTO EM BRANCO</div>';
+  lateral.innerHTML = "";
 }
-
 
 function corrige() {
   comecarEtapa();
 }
 
+let chegouNoFinal = false;
+function corrige() {
+  if (!chegouNoFinal) {
+    comecarEtapa();
+  }
+}
 function confirma() {
+  if (!chegouNoFinal) { // verifica se já chegou no final 
     let etapa = etapas[etapaAtual];
 
     let votoConfirmado = false;
+    if (votoBranco === true) {
+      votoConfirmado = true;
+      votos.push({
+        etapa: etapas[etapaAtual].titulo,
+        voto: "branco",
+      });
+    } else if (numero.length === etapa.numeros) {
+      votoConfirmado = true;
+      votos.push({
+        etapa: etapas[etapaAtual].titulo,
+        voto: numero,
+      });
+    }
 
-    if(votoBranco === true) {
-        votoConfirmado = true;
-        votos.push({
-            etapa: etapas[etapaAtual].titulo,
-            voto:'branco'
-        });
-    } else if(numero.length === etapa.numeros) {
-        votoConfirmado = true;
-        votos.push({
-            etapa: etapas[etapaAtual].titulo,
-            voto: numero
-        });
+    if (votoConfirmado) {
+      etapaAtual++;
+
+      if (etapas[etapaAtual] !== undefined) {
+        comecarEtapa();
+      } else {
+        document.querySelector(".tela").innerHTML =
+          '<div class="aviso--gigante pisca">FIM</div>';
+        console.log(votos);
+        chegouNoFinal = true;
+      }
+    }
   }
-
-  if(votoConfirmado) {
-    etapaAtual++;
-   if(etapas[etapaAtual] !== undefined) {
-    comecarEtapa();
-   }else {
-    document.querySelector('.tela').innerHTML = '<div class="aviso--gigante pisca">FIM</div>';
-    console.log(votos)
-   }
-  }
-
 }
 
-comecarEtapa();
+function corrige() {
+  if (!chegouNoFinal) {
+    comecarEtapa();
+  }
+}
+
+comecarEtapa()
